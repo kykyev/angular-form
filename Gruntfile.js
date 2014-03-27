bb3b6752a65e7203e591b6b55a5d5fd6e5c4504a
Kykyev <kykyev.kykyev@gmail.com>
2014-03-27 17:58:00 +0400

    update Gruntfile to work with Compass

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,18 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         // tasks
+        compass: {
+            dev: {
+                options: {
+                    config: 'compass.conf.rb'
+                }
+            }
+        },
         watch: {
+            sass: {
+                files: './static/sass/**/*.scss',
+                tasks: ['compass:dev']
+            },
             source: {
                 files: ['static/**', 'templates/**', 'forms.py'],
                 options: {
@@ -13,5 +24,6 @@ module.exports = function(grunt) {
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-watch');
 };
