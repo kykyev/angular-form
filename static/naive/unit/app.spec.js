aed1cfa9bcc0c74886d76864adfc05b1715e0302
Kykyev <kykyev.kykyev@gmail.com>
2014-04-03 12:06:26 +0400

    split specs

--- static/naive/unit/app.spec.js
+++ static/naive/unit/app.spec.js
@@ -1,8 +1,8 @@
 /*global jasmine, describe, it, before, beforeEach, after, afterEach, expect, module, inject */
 
-describe("FormCtrl", function() {
-    "use strict";
+"use strict";
 
+describe("FormCtrl", function() {
     var scope, ctrl, injector;
 
     beforeEach(module("myApp"));
@@ -83,66 +83,5 @@ describe("FormCtrl", function() {
             expect(scope.alias_error_message)
                 .toEqual('Someone already has the same alias');
         });
-    });
-});
-
-describe("StreamService", function () {
-    "use strict";
-
-    var stream, $httpBackend;
-
-    beforeEach(module("myApp"));
-
-    beforeEach(inject(function(StreamService, $injector) {
-        stream = StreamService.instance();
-        $httpBackend = $injector.get('$httpBackend');
-    }));
-
-    it("on successful response delivers response data", function () {
-        var spy = jasmine.createSpy();
-
-        $httpBackend.expectGET('/some/url').respond(200, "some data");
-        stream.request('/some/url').then(spy);
-        $httpBackend.flush();
-
-        expect(spy).toHaveBeenCalledWith("some data");
-    });
-
-    it("if response fails error callback is invoked", function () {
-        var spy = jasmine.createSpy();
-
-        $httpBackend.expectGET('/some/url').respond(500, "timeout");
-        stream.request('/some/url').then(function() {}, spy);
-        $httpBackend.flush();
-
-        expect(spy).toHaveBeenCalledWith("timeout");
-    });
-
-    it("if second request fired while first request is pending then first one is forgotten", function () {
-        var spy1 = jasmine.createSpy(),
-            spy2 = jasmine.createSpy(),
-            spy3 = jasmine.createSpy();
-
-        $httpBackend.when('GET', '/some/url').respond(200, "some data");
-
-        stream.request('/some/url').then(spy1, spy2);
-        stream.request('/some/url').then(spy3);
-        $httpBackend.flush();
-
-        expect(spy1).not.toHaveBeenCalled();
-        expect(spy2).not.toHaveBeenCalled();
-        expect(spy3).toHaveBeenCalledWith("some data");
-    });
-
-    it("subsequent request works as expected", function () {
-        var spy = jasmine.createSpy();
-
-        $httpBackend.when('GET', '/some/url').respond(200, "some data");
-
-        stream.request('/some/url');
-        stream.request('/some/url').then(spy);
-        $httpBackend.flush();
-
-        expect(spy).toHaveBeenCalledWith("some data");
     });
 });
